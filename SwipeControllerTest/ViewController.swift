1659821e1b5c3412f7c8a48f57b80f9622df2e12
Ronny Hendrickx <ron.hendrickx@gmail.com>
2016-02-05 11:07:22 -0700

    Initial Commit

---
+++ SwipeControllerTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwipeControllerTest
+//
+//  Created by Ronny Hendrickx on 2/5/16.
+//  Copyright © 2016 Ronny Hendrickx. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
